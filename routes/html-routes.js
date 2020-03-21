aec2cc6fdcb5b3b34139b3558b3b06deecc3ca51
Kellen Presta <kpresta04@gmail.com>
2020-03-21 10:48:36 -0500

    scores

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,6 @@ var path = require("path");
 // Routes
 // =============================================================
 module.exports = function(app) {
-
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads index.html
@@ -16,9 +15,8 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/play.html"));
   });
 
-    // scores route loads scores.html
-    app.get("/play", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/scores.html"));
-    });
-
+  // scores route loads scores.html
+  app.get("/scores", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/scores.html"));
+  });
 };
