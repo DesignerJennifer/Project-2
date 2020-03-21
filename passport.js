891b9398c0020b2fa2c6baa9da5e681b0dce849e
Matt Clark <matt.clark5991@gmail.com>
2020-03-21 13:13:17 -0500

    eslint ignore bundle

--- passport.js
+++ passport.js
@@ -1,4 +1,3 @@
-
 // config/passport.js
 
 // load all the things we need
@@ -7,9 +6,9 @@ var LocalStrategy = require('passport-local').Strategy
 var mysql = require('mysql')
 
 var connection = mysql.createConnection({
-				  host: 'localhost',
-				  user: 'root',
-				  password: 'mclark1250'
+  host: 'localhost',
+  user: 'root',
+  password: 'mclark1250'
 })
 
 connection.query('USE blackjackApp')
@@ -34,45 +33,53 @@ module.exports = function (passport) {
     })
   })
 
- 	// =========================================================================
+  // =========================================================================
   // LOCAL SIGNUP ============================================================
   // =========================================================================
   // we are using named strategies since we have one for login and one for signup
   // by default, if there was no name, it would just be called 'local'
 
-  passport.use('local-signup', new LocalStrategy({
-    // by default, local strategy uses username and password, we will override with email
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true // allows us to pass back the entire request to the callback
-  },
-  function (req, email, password, done) {
-    // find a user whose email is the same as the forms email
-    // we are checking to see if the user trying to login already exists
-    connection.query("select * from users where email = '" + email + "'", function (err, rows) {
-      console.log(rows)
-      console.log('above row object')
-      if (err) { return done(err) }
-			 if (rows.length) {
-        return done(null, false, req.flash('signupMessage', 'That email is already taken.'))
-      } else {
-        // if there is no user with that email
-        // create the user
-        var newUserMysql = new Object()
+  passport.use(
+    'local-signup',
+    new LocalStrategy(
+      {
+        // by default, local strategy uses username and password, we will override with email
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+      },
+      function (req, email, password, done) {
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        connection.query("select * from users where email = '" + email + "'", function (err, rows) {
+          console.log(rows)
+          console.log('above row object')
+          if (err) {
+            return done(err)
+          }
+          if (rows.length) {
+            return done(null, false, req.flash('signupMessage', 'That email is already taken.'))
+          } else {
+            // if there is no user with that email
+            // create the user
+            var newUserMysql = new Object()
 
-        newUserMysql.email = email
-        newUserMysql.password = password // use the generateHash function in our user model
+            newUserMysql.email = email
+            newUserMysql.password = password // use the generateHash function in our user model
 
-        var insertQuery = "INSERT INTO users ( email, password ) values ('" + email + "','" + password + "')"
-        console.log(insertQuery)
-        connection.query(insertQuery, function (err, rows) {
-          newUserMysql.id = rows.insertId
+            var insertQuery =
+                            "INSERT INTO users ( email, password ) values ('" + email + "','" + password + "')"
+            console.log(insertQuery)
+            connection.query(insertQuery, function (err, rows) {
+              newUserMysql.id = rows.insertId
 
-          return done(null, newUserMysql)
+              return done(null, newUserMysql)
+            })
+          }
         })
       }
-    })
-  }))
+    )
+  )
 
   // =========================================================================
   // LOCAL LOGIN =============================================================
@@ -80,24 +87,34 @@ module.exports = function (passport) {
   // we are using named strategies since we have one for login and one for signup
   // by default, if there was no name, it would just be called 'local'
 
-  passport.use('local-login', new LocalStrategy({
-    // by default, local strategy uses username and password, we will override with email
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true // allows us to pass back the entire request to the callback
-  },
-  function (req, email, password, done) { // callback with email and password from our form
-    connection.query("SELECT * FROM `users` WHERE `email` = '" + email + "'", function (err, rows) {
-      if (err) { return done(err) }
-			 if (!rows.length) {
-        return done(null, false, req.flash('loginMessage', 'No user found.')) // req.flash is the way to set flashdata using connect-flash
+  passport.use(
+    'local-login',
+    new LocalStrategy(
+      {
+        // by default, local strategy uses username and password, we will override with email
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+      },
+      function (req, email, password, done) {
+        // callback with email and password from our form
+        connection.query("SELECT * FROM `users` WHERE `email` = '" + email + "'", function (err, rows) {
+          if (err) {
+            return done(err)
+          }
+          if (!rows.length) {
+            return done(null, false, req.flash('loginMessage', 'No user found.')) // req.flash is the way to set flashdata using connect-flash
+          }
+
+          // if the user is found but the password is wrong
+          if (!(rows[0].password == password)) {
+            return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'))
+          } // create the loginMessage and save it to session as flashdata
+
+          // all is well, return successful user
+          return done(null, rows[0])
+        })
       }
-
-      // if the user is found but the password is wrong
-      if (!(rows[0].password == password)) { return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')) } // create the loginMessage and save it to session as flashdata
-
-      // all is well, return successful user
-      return done(null, rows[0])
-    })
-  }))
+    )
+  )
 }
