c9cb40c39ba5a437e2ef887d80326d52fea1b286
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-03-26 18:35:08 -0500

    updates to html documents

--- public/scripts.js
+++ public/scripts.js
@@ -1,12 +1,5 @@
-var body = document.body;
 
 
-var suits = ["spades", "diamonds", "clubs", "hearts"];
-var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
-var playerName = document.createElement("#human");
-
-
-
-
-playerName.textContent = "`${Human}`";
+$(document).ready(function () {
+    $(".modal").modal();
+});
